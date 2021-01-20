6cc4da1a7a07c4b60b519963728be14ced135621
loricur321 <loricur@gmail.com>
2021-01-20 10:29:50 +0100

    Primo commit

---
+++ AS2021_TPCIT_4H_CurziLorenzo_frigoriferismart/Models/Prodotti.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AS2021_TPCIT_4H_CurziLorenzo_frigoriferismart.Models
+{
+    class Prodotti
+    {
+        string _codice; //codice identificativo
+        string _descrizione; //descrizione breve
+        int _dataScadenza; //data di scadenza
+        double _calorie; //calorie del prodotto
+        double _quantita; //quantità del prodotto presente nel frigorifero
+        static int nProdotti = 0; //variabile statica che conta quanti prodotti vengono creati
+
+        public string Codice { get => _codice; set => _codice = value; }
+
+        public string Descrizione { get => _descrizione; set => _descrizione = value; }
+
+        public int DataScadenza { get => _dataScadenza; set => _dataScadenza = value; }
+
+        public double Calorie { get => _calorie; set => _calorie = value; }
+
+        public double Quantita { get => _quantita; set => _quantita = value; }
+
+
+        //costruttore default
+        //ogni volta che richiamo un costruttore richiamo il metodo COntaProdotti in modo da poter contare gli oggetti creati
+        public Prodotti () 
+        {
+            ContaProdotti();
+        }
+
+        public Prodotti (string codice, string descrizione, int dataScadenza, double calorie, double quantita)
+        {
+            ContaProdotti();
+
+            Codice = codice;
+            Descrizione = descrizione;
+            DataScadenza = dataScadenza;
+            Calorie = calorie;
+            Quantita = quantita;
+        }
+
+        //metodo che conta gli oggetti Prodotti creati
+        private void ContaProdotti ()
+        {
+            //aggiorno la variabile statica
+            nProdotti++;
+        }
+
+        //Metodo che visualizza tutti i prodotti presenti all'interno del frigorifero
+        public string VisualizzaProdotti()
+        {
+            return $"La quantità del prodotto {Descrizione} con codice {Codice}, data di scadenza {DataScadenza} e calorie {Calorie} è: {Quantita}"; 
+        }
+
+        public string VisualizzaProdottiScaduti()
+        {
+            string retVal = "";
+            if (this.DataScadenza < 2020)
+                retVal = $"Il prodotto {Descrizione} con codice {Codice} è scaduto";
+            else
+                retVal = $"Il prodotto {Descrizione} con codice {Codice} non è scaduto";
+            return retVal;
+        }
+
+        public bool Calcoloprodotti(string codice)
+        {
+            bool retVal = false;
+            
+            if(this.Codice == codice)
+                retVal = true;
+
+            return retVal;
+        }
+
+        public override string ToString()
+        {
+            return $"Prodotto {Descrizione} con codice {Codice}, data di scadenza {DataScadenza}, Calorie {Calorie} e quantità {Quantita}";
+        }
+    }
+}
---
+++ AS2021_TPCIT_4H_CurziLorenzo_frigoriferismart/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using AS2021_TPCIT_4H_CurziLorenzo_frigoriferismart.Models;
+
+namespace AS2021_TPCIT_4H_CurziLorenzo_frigoriferismart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programma frigoriferi smart di Lorenzo Curzi, 4H");
+
+            //creo un array contentente 4 prodotti
+            Prodotti[] products = new Prodotti[4];
+
+            products[0] = new Prodotti("AAA123", "Mele", 2021, 50, 5);
+            products[1] = new Prodotti("AAA124", "Carne", 2021, 1400, 1);
+            products[2] = new Prodotti("AAA125", "Pesce", 2019, 600, 2);
+            products[3] = new Prodotti("AAA126", "Pasta", 2025, 900, 7);
+
+            Console.WriteLine("Lista dei prodotti all'interno del frigorifero");
+            Console.WriteLine();
+            //Visualizzo tutti i prodotti all'interno del frigorifero
+            for(int i = 0; i < products.Length; i++)
+            {
+                Console.WriteLine(products[i].VisualizzaProdotti());
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Lista dei prodotti scaduti all'interno del frigorifero");
+            Console.WriteLine();
+            //Visualizzo eventuali prodotti scaduti
+            for (int i = 0; i < products.Length; i++)
+            {
+                Console.WriteLine(products[i].VisualizzaProdottiScaduti());
+            }
+
+            Console.WriteLine();
+            //Calcolo dei prodotti presente inserito il codice
+            Console.WriteLine("Inserire il codice del prodotto che si vuole cercare:");
+            string codice = Console.ReadLine();
+            int j = - 1;
+
+            for (int i = 0; i < products.Length; i++)
+            {
+                bool ris = products[i].Calcoloprodotti(codice);
+
+                if (ris)
+                    j = i; //salvo la posizione in cui si trova il prodotto cercato
+            }
+
+            if (j != -1)
+                Console.WriteLine($"Il prodotto cercato è: {products[j].ToString()}");
+            else
+                Console.WriteLine("Prodotto non trovato.");
+        }
+    }
+}
